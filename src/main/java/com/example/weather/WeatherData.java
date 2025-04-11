e1cc49296f506ad50d637416b4d546728abe105d
katekiselevich <kiselkath@gmail.com>
2025-04-11 16:30:36 +0200

    Add temperature unit toggle (Celsius/Fahrenheit)

--- src/main/java/com/example/weather/WeatherData.java
+++ src/main/java/com/example/weather/WeatherData.java
@@ -2,12 +2,19 @@ package com.example.weather;
 
 public class WeatherData {
     private double temperature;
+    private boolean isCelsius = true; // Новое поле
 
-    public WeatherData(double temperature) {
+    public WeatherData(double temperature, boolean isCelsius) {
         this.temperature = temperature;
+        this.isCelsius = isCelsius;
     }
 
     public double getTemperature() {
-        return temperature;
+        if (isCelsius) return temperature;
+        else return temperature * 9/5 + 32; // Перевод в Фаренгейты
+    }
+
+    public void setUnit(boolean isCelsius) {
+        this.isCelsius = isCelsius;
     }
 }
