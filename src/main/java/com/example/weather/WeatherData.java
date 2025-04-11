a8dd5bcbcf127d4ea6531a490f46ad8df9034625
mennaelnemr99 <94915628+mennaelnemr99@users.noreply.github.com>
2025-04-11 16:30:48 +0200

    add variable

--- src/main/java/com/example/weather/WeatherData.java
+++ src/main/java/com/example/weather/WeatherData.java
@@ -2,6 +2,7 @@ package com.example.weather;
 
 public class WeatherData {
     private double temperature;
+    private int temp;
 
     public WeatherData(double temperature) {
         this.temperature = temperature;
